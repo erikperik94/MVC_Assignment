38f7fde3aa287238954083b92888f633ce698413
erikperik94 <aggfelt94@hotmail.com>
2021-08-18 20:46:01 +0200

    first commit

---
+++ MVC_Assignment1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Assignment1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Home of Erik Info Page";
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewBag.Message = "About Erik";
+            return View();        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Message = "Erik Contact info";
+            return View();
+        }
+        public IActionResult Projects()
+        {
+            ViewBag.Message = "Erik Projects";
+            return View();
+        }
+    }
+}
---
+++ MVC_Assignment1/Models/Pie.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Assignment1.Models
+{
+    public class Pie
+    {
+    }
+}
